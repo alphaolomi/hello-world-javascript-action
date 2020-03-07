af73b1b7bbdb2ec1f13b115aa92ae528dfa07e23
Alpha Olomi <alphaolomi@gmail.com>
2020-03-07 20:40:51 +0300

    style: lint with prettier defautl

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const github = require("@actions/github");
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
+
+
   console.log(`Hello ${nameToGreet}!`);
   const time = new Date().toTimeString();
+  
+  // make output
   core.setOutput("time", time);
+  
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
+  
+  // 
   console.log(`The event payload: ${payload}`);
+
 } catch (error) {
+  // incase of error
   core.setFailed(error.message);
 }
